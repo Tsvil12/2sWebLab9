7d4446bf20a3d850188b63024e9888065b647987
Tsvil12 <pachacha123a@gmail.com>
2026-06-04 01:15:20 +0700

    2. добавлен @EnableCaching и базовая конфигурация кэша

--- src/main/kotlin/com/example/springflyway/SpringFlywayApplication.kt
+++ src/main/kotlin/com/example/springflyway/SpringFlywayApplication.kt
@@ -2,8 +2,10 @@ package com.example.springflyway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
 @SpringBootApplication
+@EnableCaching
 open class SpringFlywayApplication
 
 fun main(args: Array<String>) {
